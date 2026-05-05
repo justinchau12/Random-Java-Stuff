b4b36236e764d95f1603b921b650f73d08375a2f
Justin <jc.justinchau@gmail.com>
2026-05-05 19:39:54 -0400

    Add files via upload

--- SpiralDot/Spiral.java
+++ SpiralDot/Spiral.java
@@ -6,75 +6,39 @@ import java.awt.event.*;
 public class Spiral extends JPanel implements ActionListener{
     
     // Constant
+    private double spiralRadius = 0;
     private double angle = 0;
-    private double radius = 0;
     private double deltaAngle = 0.05;
     private double deltaRadius = 0.05;
-    private int timerMS = 10;
+    private double particleRadius = 15.0;
+    private int timerMS = 16;
     
-    private double fw;
-    private double fh;
-    private Dot d;
+    private double centerX, centerY, pointX, pointY;
     private Timer timer;
     
-    public Spiral(int fw, int fh){
-        this.fw = fw;
-        this.fh = fh;
+    public Spiral(int frameWidth, int frameHeight){
         this.timer = new Timer(timerMS, this);
         this.timer.start();
-        d = new Dot(fw, fh);
+
+        centerX = frameWidth / 2.0;
+        centerY = frameHeight / 2.0;
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.RED);
-        g.fillOval((int)d.getX(), (int)d.getY(), (int)d.getRadius(), (int)d.getRadius());
+        g.fillOval((int)(pointX - particleRadius), (int)(pointY - particleRadius), (int)(particleRadius * 2.0), (int)(particleRadius * 2.0));
     }
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        d.setX((fw - d.getRadius())/2.0 + Math.cos(angle)*radius);
-        d.setY((fh - d.getRadius())/2.0 + Math.sin(angle)*radius);
+        pointX = centerX + Math.cos(angle) * spiralRadius;
+        pointY = centerY + Math.sin(angle) * spiralRadius;
         angle %= (2 * Math.PI);
         angle += deltaAngle;
-        radius += deltaRadius;
+        spiralRadius += deltaRadius;
         
         repaint();
-    }
-}
-
-class Dot{
-    // Constant
-    private double radius = 30.0;
-    
-    private double x;
-    private double y;
-    
-    public Dot(int fw, int fh){
-        this.x = fw/2.0;
-        this.y = fh/2.0;
-    }
-    
-    public double getX(){
-        return x;
-    }
-    
-    public void setX(double newX){
-        x = newX;
-        return;
-    }
-    
-    public double getY(){
-        return y;
-    }
-    
-    public void setY(double newY){
-        y = newY;
-        return;
-    }
-    
-    public double getRadius(){
-        return radius;
     }
 }
